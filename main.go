c83b025d0ef3862d64f9d5f352fc506bcc7723c0
Dany <danymike.joseph@gmail.com>
2022-09-26 16:04:01 +0200

    feat: getItems mock endpoint created

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type item struct {
-	Id       string
-	name     string
-	value    string
-	quantity int
+	ID       string
+	Name     string
+	Value    int
+	Quantity int
+}
+
+var items = []item{
+	{ID: "1", Name: "VOLKSWAGEN POLO 6", Value: 15800, Quantity: 1},
+	{ID: "2", Name: "Iphone 14", Value: 1000, Quantity: 1},
+}
+
+func getItems(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, items)
 }
 
 func main() {
 	router := gin.Default()
+	router.GET("/items", getItems)
 	router.Run("localhost:9090")
 }
