ed7938693afd775a6f4b42d882e2b9babc2950c7
Dany <danymike.joseph@gmail.com>
2022-10-03 16:53:58 +0200

    feat: connect db to golang API

--- main.go
+++ main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
-	routes "golang_net_worth_calculator_api/routes"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	db "golang_net_worth_calculator_api/database"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db.Open()
+	sqlDB, _ := db.DB.DB()
+
+	defer sqlDB.Close()
+
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hello world!"})
+	})
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "9090"
 	}
-	router := gin.New()
-	router.Use(gin.Logger())
-
-	routes.AuthRoutes(router)
-	routes.UserRoutes(router)
+	// routes.AuthRoutes(router)
+	// routes.UserRoutes(router)
 	router.Run("localhost:9090")
+	fmt.Println("Established a successful connection!")
 }
