01b87a7483b5292345acebfab5d697dd4cbc94b8
Dany <danymike.joseph@gmail.com>
2022-10-05 22:53:34 +0200

    feat: add User model

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
 	db "golang_net_worth_calculator_api/database"
+	routes "golang_net_worth_calculator_api/routes"
 )
 
 func main() {
@@ -20,20 +19,11 @@ func main() {
 
 	db.Open()
 	sqlDB, _ := db.DB.DB()
-
 	defer sqlDB.Close()
 
 	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "hello world!"})
-	})
 
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "9090"
-	}
-	// routes.AuthRoutes(router)
+	routes.AuthRoutes(router)
 	// routes.UserRoutes(router)
 	router.Run("localhost:9090")
 	fmt.Println("Established a successful connection!")
