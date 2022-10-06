9ae564263b6eba64774edc969bcc879adf9d3b76
Dany <danymike.joseph@gmail.com>
2022-10-06 15:46:25 +0200

    feat: protect route

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -8,3 +8,7 @@ import (
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
 	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
 }
+
+func (server *Server) HomeProtected(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, "Protected Route")
+}
--- controllers/routes.go
+++ controllers/routes.go
@@ -5,6 +5,7 @@ import "golang_net_worth_calculator_api/middlewares"
 func (s *Server) initializeRoutes() {
 
 	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+	s.Router.HandleFunc("/protected", middlewares.SetMiddlewareAuthentication(s.HomeProtected)).Methods("GET")
 
 	// Login Route
 	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
