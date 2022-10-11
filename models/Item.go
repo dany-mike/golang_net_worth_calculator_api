e765e8c2a23d6bb91ef415602c2bec80763e0075
Dany <danymike.joseph@gmail.com>
2022-10-11 09:49:54 +0200

    Item model created

---
+++ models/Item.go
@@ -0,0 +1,121 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Item struct {
+	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
+	Title     string    `gorm:"size:255;not null;unique" json:"title"`
+	Price     string    `gorm:"size:255;not null;" json:"price"`
+	User      User      `json:"user"`
+	UserID    uint32    `gorm:"not null" json:"user_id"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (p *Item) Prepare() {
+	p.ID = 0
+	p.Title = html.EscapeString(strings.TrimSpace(p.Title))
+	p.Price = html.EscapeString(strings.TrimSpace(p.Price))
+	p.User = User{}
+	p.CreatedAt = time.Now()
+	p.UpdatedAt = time.Now()
+}
+
+func (p *Item) Validate() error {
+
+	if p.Title == "" {
+		return errors.New("required title")
+	}
+	if p.Price == "" {
+		return errors.New("required price")
+	}
+	if p.UserID < 1 {
+		return errors.New("required user")
+	}
+	return nil
+}
+
+func (p *Item) SaveItem(db *gorm.DB) (*Item, error) {
+	var err error
+	err = db.Debug().Model(&Item{}).Create(&p).Error
+	if err != nil {
+		return &Item{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.UserID).Take(&p.User).Error
+		if err != nil {
+			return &Item{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Item) FindAllItems(db *gorm.DB) (*[]Item, error) {
+	var err error
+	items := []Item{}
+	err = db.Debug().Model(&Item{}).Limit(100).Find(&items).Error
+	if err != nil {
+		return &[]Item{}, err
+	}
+	if len(items) > 0 {
+		for i, _ := range items {
+			err := db.Debug().Model(&User{}).Where("id = ?", items[i].UserID).Take(&items[i].User).Error
+			if err != nil {
+				return &[]Item{}, err
+			}
+		}
+	}
+	return &items, nil
+}
+
+func (p *Item) FindItemByID(db *gorm.DB, pid uint64) (*Item, error) {
+	var err error
+	err = db.Debug().Model(&Item{}).Where("id = ?", pid).Take(&p).Error
+	if err != nil {
+		return &Item{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.UserID).Take(&p.User).Error
+		if err != nil {
+			return &Item{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Item) UpdateAItem(db *gorm.DB) (*Item, error) {
+
+	var err error
+
+	err = db.Debug().Model(&Item{}).Where("id = ?", p.ID).Updates(Item{Title: p.Title, User: p.User, UpdatedAt: time.Now()}).Error
+	if err != nil {
+		return &Item{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.UserID).Take(&p.User).Error
+		if err != nil {
+			return &Item{}, err
+		}
+	}
+	return p, nil
+}
+
+func (p *Item) DeleteAItem(db *gorm.DB, pid uint64, uid uint32) (int64, error) {
+
+	db = db.Debug().Model(&Item{}).Where("id = ? and user_id = ?", pid, uid).Take(&Item{}).Delete(&Item{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Item not found")
+		}
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
